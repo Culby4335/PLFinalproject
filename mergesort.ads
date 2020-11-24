ef0ade22bf1e52a071be88e81d1ddfaea9e97912
Culby4335 <46946502+Culby4335@users.noreply.github.com>
2020-11-24 09:37:44 -0700

    Add files via upload

--- mergesort.ads
+++ mergesort.ads
@@ -1,12 +1,5 @@
 with data;
-use data; 
-
---generic
-   --type Element_Type is private;
-   --type Collection_Type is array(Index_Type range <>) of Element_Type;
-   --with function "<"(Left, Right : Element_Type) return Boolean is <>;
-   --with function "<="(Left, Right : Element_Type) return Boolean is <>;
- 
+use data;
 package sortAlg is
    function doSort(Item : Collection_Type) return Collection_Type;
 end sortAlg;
